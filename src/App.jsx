e8a734816e7354e409166ac9e195550734c47cbf
tchkoidze <ilia.chkoidze12@gmail.com>
2023-11-02 15:08:37 +0400

    feat: install react router dom, create landing page file

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import Landing from "./pages/Landing";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Landing = () => {
+  return <Main></Main>;
+};
+
+export default Landing;
+
+const Main = styled.main`
+  width: 100%;
+  height: 100%;
+  background-color: red;
+`;
